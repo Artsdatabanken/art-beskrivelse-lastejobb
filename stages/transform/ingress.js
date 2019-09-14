d693e1875209b9424e7bb94f459953c34de2284d
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-09-14 13:47:10 +0200

    Strip out html

---
+++ stages/transform/ingress.js
@@ -0,0 +1,33 @@
+const { io } = require("lastejobb");
+
+const ingress = io.lesDatafil("tekst");
+const r = {};
+
+Object.keys(ingress).forEach(key => {
+  r[key] = cleanup(ingress[key]);
+});
+io.skrivBuildfil(__filename, r);
+
+function cleanup(e) {
+  clean(e, "ingress");
+  clean(e, "brødtekst");
+  return e;
+}
+
+function clean(e, key) {
+  const item = e[key];
+  if (!item) return;
+  Object.keys(item).forEach(key => {
+    const v = item[key];
+    item[key] = stripHtml(v);
+  });
+}
+
+function stripHtml(v) {
+  v = v.replace(/\<\/?em\>/g, "");
+  v = v.replace(/\<\/?strong\>/g, "");
+  v = v.replace(/\<\/?p\>/g, "");
+  v = v.replace(/\<a href=\"(.*?)\">.*<\/a>/g, "$1");
+  v = v.replace(/\n/g, "");
+  return v.trim();
+}
